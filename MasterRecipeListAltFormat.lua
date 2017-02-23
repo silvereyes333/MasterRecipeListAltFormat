f3a4e6e7003353f3be908f36ac7c061a94fbbdbd
silvereyes <silvereyes333@gmail.com>
2017-02-23 10:26:08 -0600

    Version 1.1.0
    - Added support for popup tooltips
    - Changed color for known alts to match original ESO MRL addon colors
    - Code cleanup and commenting for clarity

--- MasterRecipeListAltFormat.lua
+++ MasterRecipeListAltFormat.lua
@@ -3,17 +3,28 @@ local addon = {
     name = addonName,
     title = "ESO Master Recipe List Alt Format",
     author = "|c99CCEFsilvereyes|r",
-    version = "1.0.0",
-    debugMode = false,
+    version = "1.1.0",
 }
-local charNames = {}
-local knownCharsByName = {}
-local COLOR_UNKNOWN = ZO_ERROR_COLOR
-local COLOR_KNOWN = ZO_ColorDef:New(GetInterfaceColor(INTERFACE_COLOR_TYPE_ITEM_QUALITY_COLORS, ITEM_QUALITY_MAGIC))
-local COLOR_INDETERMINATE = ZO_DEFAULT_DISABLED_COLOR
-local originalAddLine
-local originalAddVerticalPadding
-local function StripColor(text)
+
+-- Color configuration
+local COLOR_UNKNOWN               = ZO_ERROR_COLOR
+local COLOR_KNOWN                 = ZO_ColorDef:New("66CCFF")
+local COLOR_INDETERMINATE         = ZO_DEFAULT_DISABLED_COLOR
+
+local charNames                   = {} -- all character names, indexed by order
+local knownCharsByName            = {} -- scanned character name lookup index
+local originalAddLines            = {} -- original tooltip AddLine() methods, indexed by control
+local originalAddVerticalPaddings = {} -- original tooltip AddVerticalPadding() methods, indexed by control
+
+local reformatting       -- flag indicating we are in the middle of a reformat
+local verticalPadding    -- stores padding to be added before reformatted list
+local tooltipLineBuilder -- stores multiline alt lists in between AddLine() calls
+local knownByHeader      -- ESO Master Recipe List "Known By" header text
+local craftableByHeader  -- ESO Master Recipe List "Craftable By" header text
+
+--[[ Removes any ESO color code markers from the start and end of the given text. ]]
+local function StripColorAndWhitespace(text)
+    text = zo_strtrim(text)
     if string.sub(text, 1, 2) == "|c" then
         text = string.sub(text, 9)
     end
@@ -22,47 +33,69 @@ local function StripColor(text)
     end
     return text
 end
+
+--[[ Reformats a colorized list of characters into an ordered list, and appends any remaining 
+     characters in either red or grey, depending on whether they have been scanned by 
+     ESO Master Recipe List yet.]]
 local function ReformatKnownBy(text)
+
+    -- Parse out the character names in the passed in list
     local names = {}
     for part in string.gmatch(text, '([^,]+)') do
-        local characterName = zo_strtrim(part)
-        characterName = StripColor(characterName)
+        local characterName = StripColorAndWhitespace(part)
         names[characterName] = true
     end
+    
+    -- Create an ordered, colorized list of the names
     local formattedNames = {}
     for _, characterName in ipairs(charNames) do
         if names[characterName] then
             table.insert(formattedNames, COLOR_KNOWN:Colorize(characterName))
         end
     end
+    
+    -- Append an ordered list of any remaining names that have been scanned, colored in red
     for _, characterName in ipairs(charNames) do
         if not names[characterName] and knownCharsByName[characterName] then
             table.insert(formattedNames, COLOR_UNKNOWN:Colorize(characterName))
         end
     end
+    
+    -- Append a list of any remaining names that have not been scanned, colored in grey
     for _, characterName in ipairs(charNames) do
         if not names[characterName] and not knownCharsByName[characterName] then
             table.insert(formattedNames, COLOR_INDETERMINATE:Colorize(characterName))
         end
     end
+    
+    -- Create the final comma separated list of names
     local output = table.concat(formattedNames, ", ")
     return output
 end
-local processNextTooltipLine
-local tooltipLineBuilder
-local knownByHeader
-local craftableByHeader
-local verticalPadding
-local function ItemTooltipAddVerticalPadding(control, paddingY)
-    if processNextTooltipLine then
+
+--[[ Hooks ItemTooltip:AddVerticalPadding() and PopupTooltip:AddVerticalPadding(). 
+     When in the middle of a multi-line alt list reformat, it ignores any vertical padding calls in
+     the middle of the list, since we are going to format the whole list as a single line. 
+     The padding amount is saved in the verticalPadding local variable so that we can add it before 
+     writing the final line.]]
+local function TooltipAddVerticalPadding(control, paddingY)
+    if reformatting then
         verticalPadding = paddingY
         return
     end
-    originalAddVerticalPadding(control, paddingY)
+    originalAddVerticalPaddings[control](control, paddingY)
 end
-local function ItemTooltipAddLine(control, text, font, r, g, b, lineAnchor, modifyTextType, textAlignment, setToFullSize)
+
+--[[ Hooks ItemTooltip:AddLine() and PopupTooltip:AddLine(). 
+     Detects the "known by" and "craftable by" tooltip lines, and then reformats the following list
+     of alts that are added. ]]
+local function TooltipAddLine(control, text, font, r, g, b, lineAnchor, modifyTextType, textAlignment, setToFullSize)
     
-    if processNextTooltipLine then
+    -- We are in the middle of a reformat
+    if reformatting then
+    
+        -- Lines ending in commas indicate more lines to come, so append the text to the running
+        -- tooltipLineBuilder, and then return.
         if string.sub(text, -2) == ", " then
             if tooltipLineBuilder then
                 tooltipLineBuilder = tooltipLineBuilder .. text
@@ -72,46 +105,66 @@ local function ItemTooltipAddLine(control, text, font, r, g, b, lineAnchor, modi
             return
         end
         
-        processNextTooltipLine = nil
+        -- No comma means this is the last line
+        reformatting = nil
         if tooltipLineBuilder then
             text = tooltipLineBuilder .. text
             tooltipLineBuilder = nil
         end
         
+        -- Perform the reformat
         text = ReformatKnownBy(text)
-        textAlignment = TEXT_ALIGN_CENTER
         font = ""
         lineAnchor = CENTER
+        textAlignment = TEXT_ALIGN_CENTER
         setToFullSize = true
-        if verticalPadding then
-            originalAddVerticalPadding(control, verticalPadding)
-        end
         
+        -- Add the most recently-seen vertical padding, if there was one (there should be)
+        if verticalPadding then
+            originalAddVerticalPaddings[control](control, verticalPadding)
+        end
+    
+    -- Known By or Craftable By header detected. Start reformat the next time AddLine() is called.
     elseif text == knownByHeader or text == craftableByHeader then
-        processNextTooltipLine = true
+        reformatting = true
+    
+    -- Not in a reformat. So, no special processing. Just pass through.
+    --else
     end
     
-    originalAddLine(control, text, font, r, g, b, lineAnchor, modifyTextType, textAlignment, setToFullSize)
+    -- Add the line to the tooltip
+    originalAddLines[control](control, text, font, r, g, b, lineAnchor, modifyTextType, textAlignment, setToFullSize)
 end
+
 local function OnAddonLoaded(event, name)
     if name ~= addonName then return end
+    
+    -- Populate ordered list of character names for this account
     for i = 1, GetNumCharacters() do
         local characterName = zo_strformat("<<1>>", GetCharacterInfo(i))
         charNames[i] = characterName
     end
+    
+    -- Populate list of characters that have been scanned by ESO Master Recipe List
     local accountName = GetDisplayName()
     for characterName, _ in pairs(MasterRecipeList.Default[accountName]) do
         if characterName ~= "$AccountWide" then
             knownCharsByName[characterName] = true
         end
     end
+    
+    -- Store the tooltip headers used to signal that the following line(s) represents a list of alts
     local mrlStrings = ESOMRL:GetLanguage()
     knownByHeader = mrlStrings.ESOMRL_KNOWN
     craftableByHeader = mrlStrings.ESOMRL_CRAFTABLE
-    originalAddLine = ItemTooltip.AddLine
-    originalAddVerticalPadding = ItemTooltip.AddVerticalPadding
-    ItemTooltip.AddLine = ItemTooltipAddLine
-    ItemTooltip.AddVerticalPadding = ItemTooltipAddVerticalPadding
+    
+    -- Hook tooltip AddLine and AddVerticalPadding functions
+    for _, tooltip in ipairs({ItemTooltip, PopupTooltip}) do
+        originalAddLines[tooltip] = tooltip.AddLine
+        originalAddVerticalPaddings[tooltip] = tooltip.AddVerticalPadding
+        tooltip.AddLine = TooltipAddLine
+        tooltip.AddVerticalPadding = TooltipAddVerticalPadding
+    end
 end
 EVENT_MANAGER:RegisterForEvent(addonName, EVENT_ADD_ON_LOADED, OnAddonLoaded)
 MasterRecipeListAltFormat = addon
