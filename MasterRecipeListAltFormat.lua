2e90885ff2039d512b06f37482ff2068644c212d
silvereyes <silvereyes333@gmail.com>
2021-02-14 16:07:27 -0600

    Version 1.1.14
    - Update 29 Flames of Ambition compatibility
    - This version is compatible with both live (Markarth) and PTS (Flames of Ambition)

--- MasterRecipeListAltFormat.lua
+++ MasterRecipeListAltFormat.lua
@@ -3,7 +3,7 @@ local addon = {
     name = addonName,
     title = "ESO Master Recipe List Alt Format",
     author = "silvereyes",
-    version = "1.1.13",
+    version = "1.1.14",
 }
 
 -- Color configuration
